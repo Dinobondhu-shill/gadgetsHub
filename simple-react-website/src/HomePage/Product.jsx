cf7e43dbd3b280e17efa718e9229f0ee1f8c73d7
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-09-25 17:56:44 +0600

    dynamically product card added

--- simple-react-website/src/HomePage/Product.jsx
+++ simple-react-website/src/HomePage/Product.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ProductCard from '../components/ProductCard';
 
 const Product = () => {
-const [product, setProduct] = useState([]);
+const [products, setProduct] = useState([]);
 
 useEffect(()=>{
   const fetchProduct = async()=>{
@@ -20,7 +20,11 @@ useEffect(()=>{
   return (
     <div>
       <h2 className='text-center font-bold underline text-3xl my-5 text-[#25a585]'>Our Gadgets</h2>
-      <ProductCard product={product}/>
+     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+     {
+        products?.map(product => <ProductCard key={product.id} product={product}/>)
+      }
+     </div>
     </div>
   );
 };
--- simple-react-website/src/components/ProductCard.jsx
+++ simple-react-website/src/components/ProductCard.jsx
@@ -1,10 +1,13 @@
 
 
-const ProductCard = (product) => {
-  console.log(product)
+const ProductCard = ({product}) => {
+  const {name, image, description, price} = product
   return (
     <div className="border border-[#5ee6c4] rounded-md p-3 ">
-      
+      <img src={image} alt={name} className="w-full h-48 object-cover rounded-md" />
+      <h2 className="text-center text-2xl font-medium my-4">{name}</h2>
+      <p className="text-center">Price : <span className="text-[#5ee6c4]">{price}</span></p>
+      <p>{description}</p>
     </div>
   );
 };
